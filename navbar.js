ca87aec0f826cfc329d9cc114c36769f17ed9e7b
Koumori97 <d.e.fledermaus86@gmail.com>
2021-06-15 10:50:40 +0200

    updated

--- navbar.js
+++ navbar.js
@@ -1,3 +1,10 @@
 function toggleNavbar() {
   document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
+  if (nav_icon.className == "fas fa-bars") {
+    nav_icon.className = "fas fa-times";
+  } else if (nav_icon.className == "fas fa-times") {
+    nav_icon.className = "fas fa-bars";
+  }
 }
+
+let nav_icon = document.querySelector(".fas");
